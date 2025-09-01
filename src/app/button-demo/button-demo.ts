c86befa42f963b8422866f2ba20fac4cbd872628
Mohamedou <medou.pha@gmail.Com>
2025-09-01 22:35:04 +0000

    init

---
+++ src/app/button-demo/button-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-demo',
+  imports: [],
+  templateUrl: './button-demo.html',
+  styleUrl: './button-demo.css'
+})
+export class ButtonDemo {
+
+}
